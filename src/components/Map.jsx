a6ed0f6b1aad8d34d5270eb891cb79870d185c02
Vladislav Safronov <midisar.150@mail.ru>
2025-06-27 15:51:59 +0300

    Yandex

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,14 +7,8 @@ function Map() {
 
         <div className="relative z-10  max-w-6xl mx-auto">
             <h2 className="text-3xl text-white font-bold text-center mb-8">Местоположение</h2>
-
-        
-                <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A75b1941027e0e1b3d3820f0f7fafffa16d5f28647d26f7d8dda2087915d202fd&amp;source=constructor" width="100%" height="500" frameborder="0"></iframe>
-
+            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A75b1941027e0e1b3d3820f0f7fafffa16d5f28647d26f7d8dda2087915d202fd&amp;source=constructor" width="100%" height="500" frameborder="0"></iframe>
             <h3 className="text-3xl text-white font-bold text-center pt-8">Орёл, улица 5 Августа, 54, 3 этаж</h3>
-      
-                
-            
         </div>
     </section>
   );
